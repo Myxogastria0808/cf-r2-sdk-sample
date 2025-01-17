6016ef780b15a97e5228cfcd280f4d43effa1c33
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-01-18 02:44:53 +0900

    feat: add anotation

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() {
         env::var("SECRET_ACCESS_KEY").expect("SECRET_ACCESS_KEY not found in .env file.");
     let region: String = env::var("REGION").expect("REGION not found in .env file.");
 
-    let object = Builder::new()
+    let object: cf_r2_sdk::operator::Operator = Builder::new()
         .set_bucket_name(bucket_name)
         .set_access_key_id(access_key_id)
         .set_secret_access_key(secret_access_key)
@@ -28,7 +28,7 @@ async fn main() {
         .upload_binary("text.txt", "text/plain", b"Hello, World!")
         .await;
 
-    let bin = object.download("text.txt").await;
+    let bin: Result<Vec<u8>, cf_r2_sdk::error::OperationError> = object.download("text.txt").await;
 
     println!("{:?}", bin);
 }
