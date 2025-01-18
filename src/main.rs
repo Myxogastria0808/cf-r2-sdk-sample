571dd285668b8f6091f03701ca41c995ef64a458
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-01-19 08:40:59 +0900

    faet: update sample code

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ async fn main() -> Result<(), cf_r2_sdk::error::OperationError> {
         .upload_binary("text.txt", "text/plain", b"Hello, World!")
         .await?;
 
-    let bin = object.download("text.txt").await?;
+    let bin: Vec<u8> = object.download("text.txt").await?;
 
     println!("{:?}", bin);
     Ok(())
